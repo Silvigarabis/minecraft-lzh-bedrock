aad0df30e456d8e2bcc9c20a87dac40868df8bcd
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-07-01 08:49:38 +0800

    生成了lzh_bedrock.lang

--- work/same_get_text.sh
+++ work/same_get_text.sh
@@ -1,12 +1,18 @@
-texts="$(cat java.lang)"
-while read -r key; do
-  key=($key)
-  while read -r jtext; do
-    keyj="${jtext%%=*}"
-    if [ "$keyj" = "${key[1]}" ]; then
-      echo "${key[0]}=${jtext#*=}"
-    fi
-  done <<EOM
+one(){
+        key=($key)
+        while read -r jtext; do
+                keyj="${jtext%%=*}"
+                if [ "$keyj" = "${key[1]}" ]; then
+                        echo "${key[0]}=${jtext#*=}"
+                fi
+        done <<EOM
 $texts
 EOM
+}
+texts="$(cat java.lang)"
+while read -r key; do
+        if [ "$1" = fast ]; then
+                one &
+        else one
+        fi
 done<got.txt >lzh_bedrock.lang
