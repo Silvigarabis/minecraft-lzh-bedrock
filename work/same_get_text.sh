cf84c3ad61a4f2c1fdc5936b41f45a45083faa5f
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-07-01 08:24:18 +0800

    创建了测试代码

---
+++ work/same_get_text.sh
@@ -0,0 +1,12 @@
+texts="$(cat java.lang)"
+while read -r key; do
+  key=($key)
+  while read -r jtext; do
+    keyj="${text%%=*}"
+    if [ "$keyj" = "${key[1]}" ]; then
+      echo "${key[0]}=${jtext#*=}"
+    fi
+  done <<EOM
+$texts
+EOM
+done<got >lzh_bedrock.lang
