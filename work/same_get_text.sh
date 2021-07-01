ea2816e163318e243fb1a154204ec1c15f25aba1
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2021-07-01 08:29:08 +0800

    测试成功，即将开始生成

--- work/same_get_text.sh
+++ work/same_get_text.sh
@@ -2,11 +2,11 @@ texts="$(cat java.lang)"
 while read -r key; do
   key=($key)
   while read -r jtext; do
-    keyj="${text%%=*}"
+    keyj="${jtext%%=*}"
     if [ "$keyj" = "${key[1]}" ]; then
       echo "${key[0]}=${jtext#*=}"
     fi
   done <<EOM
 $texts
 EOM
-done<got >lzh_bedrock.lang
+done<got.txt >lzh_bedrock.lang
